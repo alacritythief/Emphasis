31bf4237aa8c1948a661ac1cb8d61fef419c213d
Andy Wong <alacritythief@gmail.com>
2014-10-09 00:00:03 -0400

    fixed pagination bug, added validations

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,8 +3,8 @@ class Page < ActiveRecord::Base
   belongs_to :user
   has_many :elements
 
-  validates :chapter, numericality: { greater_than_or_equal_to: 0 }, presence: true, uniqueness: { scope: [ :comic_id, :number ]  }
-  validates :number, numericality: { greater_than_or_equal_to: 0 }, presence: true, uniqueness: { scope: [ :comic_id, :chapter ]  }
+  validates :chapter, numericality: { greater_than_or_equal_to: 1 }, presence: true, uniqueness: { scope: [ :comic_id, :number ]  }
+  validates :number, numericality: { greater_than_or_equal_to: 1 }, presence: true, uniqueness: { scope: [ :comic_id, :chapter ]  }
   validates :page_type, presence: true, length: { maximum: 10 }
 
   def self.authorized_find(user, id)
