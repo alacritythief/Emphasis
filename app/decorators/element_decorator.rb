6cb3f734b9002f3a11e161bc855cc5a943052c39
Andy Wong <alacritythief@gmail.com>
2014-10-14 22:47:37 -0400

    refactored interpreter

--- app/decorators/element_decorator.rb
+++ app/decorators/element_decorator.rb
@@ -2,43 +2,49 @@ class ElementDecorator < Draper::Decorator
   delegate_all
 
   def command
-    name = object.id_name
-    entries = parse
-    type = entries[0][0]
-    parameters = []
+    commands = parse
+    type = commands[0][0].downcase
 
-    if entries[1][0] == "time"
-      time = entries[1][1]
+    if commands[1][0] == "time"
+      time = commands[1][1]
     else
       time = 1
     end
 
-    if type.downcase == "move"
-      entries.shift(2)
-      entries.each do |k,v|
-        if !v.nil?
-          parameters << "#{k}: #{v}"
-        else
-          parameters << k
-        end
+    if type == "move"
+      commands.shift(2)
+      move(time, commands)
+    end
+  end
+
+  def move(time, array)
+    name = object.id_name
+    parameters = []
+
+    array.each do |k,v|
+      if !v.nil?
+        parameters << "#{k}: #{v}"
+      else
+        parameters << k
       end
-      values = parameters.join(",")
-      output = "TweenMax.to(#{name}, #{time}, {#{values}});"
-      output.html_safe
     end
 
+    values = parameters.join(",")
+    output = "TweenMax.to(#{name}, #{time}, {#{values}});"
+    output.html_safe
   end
 
   def parse
-    code = object.js
-    commands_hash = {}
-    raw_commands_array = []
-    raw_script = code.gsub(/\r\n/,"").split(";")
-    raw_script.each do |command|
-      raw_commands_array << command.split(":")
+    commands_array = []
+    raw_code = object.js
+    script = raw_code.gsub(/\r\n/,"").split(";")
+
+    script.each do |command|
+      commands_array << command.split(":")
     end
-    raw_commands_array
+    commands_array
   end
+
 end
 
 # mouse example, to be implemented
