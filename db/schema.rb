5f351bce404c5120a5fbc8ac8980f0e80b58b3dc
Andy Wong <alacritythief@gmail.com>
2014-10-15 21:30:34 -0400

    index added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141014013830) do
+ActiveRecord::Schema.define(version: 20141016012657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,19 @@ ActiveRecord::Schema.define(version: 20141014013830) do
 
   add_index "elements", ["page_id"], name: "index_elements_on_page_id", using: :btree
 
+  create_table "friendly_id_slugs", force: true do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
+
   create_table "pages", force: true do |t|
     t.integer  "chapter",                     null: false
     t.string   "name"
@@ -57,6 +70,7 @@ ActiveRecord::Schema.define(version: 20141014013830) do
     t.integer  "user_id",                     null: false
   end
 
+  add_index "pages", ["chapter", "number"], name: "index_pages_on_chapter_and_number", using: :btree
   add_index "pages", ["comic_id"], name: "index_pages_on_comic_id", using: :btree
 
   create_table "users", force: true do |t|
